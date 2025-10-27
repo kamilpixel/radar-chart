84f6893c1ddceb9f454df7886c5fecb616cae56a
Kamil <48324735+kamilpixel@users.noreply.github.com>
2025-10-27 18:42:39 +0800

    feat(radar-chart): use hidden input to set pre-defined cityname. add error handler to handle invalid city

--- src/main.js
+++ src/main.js
@@ -7,28 +7,72 @@ if (ctx) {
 
   // Modify data to fit chartjs data format
   const populateData = (data) => {
-    const indexNumber = 1;
+    let indexNumber = null;
 
-    return {
-      locationLabel: `${data.chartData[indexNumber].region} / ${data.chartData[indexNumber].city_name}`,
-      chartData: {
-        labels: data.chartConfig[0].labels,
-        datasets: [
-          {
-            label: "Global Data",
-            data: data.chartData[indexNumber].global_data,
+    // Get city name from hidden input
+    const cityNameInput = document.getElementById("cityName");
+    const cityName = cityNameInput ? cityNameInput.value : "";
+
+    // Find index number based on city name
+    data.chartData.forEach((item, index) => {
+      if (item.city_name.toLowerCase() === cityName.toLowerCase()) {
+        indexNumber = index;
+      }
+    });
+
+    if (indexNumber === null) {
+      return false;
+    } else {
+      return {
+        chartData: {
+          labels: data.chartConfig[0].labels,
+          datasets: [
+            {
+              label: "Global",
+              data: data.chartData[indexNumber].global_data,
+            },
+            {
+              label: data.chartData[indexNumber].region,
+              data: data.chartData[indexNumber].region_data,
+            },
+            {
+              label: data.chartData[indexNumber].city_name,
+              data: data.chartData[indexNumber].city_data,
+            },
+          ],
+        },
+      };
+    }
+  };
+
+  // Initialize radar chart
+  const initChart = () => {
+    new Chart(ctx, {
+      type: "radar",
+      data: cleanData.chartData,
+      options: {
+        responsive: true,
+        maintainAspectRatio: true,
+        elements: {
+          line: {
+            borderWidth: 1,
           },
-          {
-            label: "Region Data",
-            data: data.chartData[indexNumber].region_data,
+        },
+        scales: {
+          r: {
+            pointLabels: {
+              font: {
+                size: 14,
+              },
+            },
+            ticks: {
+              beginAtZero: true,
+              stepSize: 20,
+            },
           },
-          {
-            label: "City Data",
-            data: data.chartData[indexNumber].city_data,
-          },
-        ],
+        },
       },
-    };
+    });
   };
 
   // Fetch data from sample json file
@@ -37,41 +81,17 @@ if (ctx) {
       let response = await fetch("/data.json");
       response = await response.json();
       cleanData = populateData(response);
-      const elLocationLabel = document.getElementById("location-label");
-      if (elLocationLabel) {
-        elLocationLabel.textContent = cleanData.locationLabel;
+      if (cleanData) {
+        initChart();
+      } else {
+        // Show error message if city not found
+        document.querySelector(".error-message").textContent =
+          "Error: City data not found";
+        document.querySelector(".chart-container").style.display = "none";
       }
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
   await fetchData();
-
-  // Initialize radar chart
-  new Chart(ctx, {
-    type: "radar",
-    data: cleanData.chartData,
-    options: {
-      responsive: true,
-      maintainAspectRatio: true,
-      elements: {
-        line: {
-          borderWidth: 1,
-        },
-      },
-      scales: {
-        r: {
-          pointLabels: {
-            font: {
-              size: 14,
-            },
-          },
-          ticks: {
-            beginAtZero: true,
-            stepSize: 20,
-          },
-        },
-      },
-    },
-  });
 }
